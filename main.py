93d893d7802470a5c8cc719d66dec71018483272
watery2 <dominykasorlovas7@gmail.com>
2021-04-05 11:12:53 +0300

    made it so you could add friends

--- main.py
+++ main.py
@@ -14,7 +14,12 @@ class User(db.Model):
     name = db.Column(db.String(50),unique=True,nullable=False)
     password = db.Column(db.String(80),unique=True,nullable=False)
     desc = db.Column(db.String,nullable=False)
-    friends = db.Column(db.PickleType, nullable=False)
+
+class Friends(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(50),unique=False,nullable=False)
+    friends = db.Column(db.String,unique=False,nullable=False)
+
 
 @app.route("/",methods=['GET', 'POST'])
 def login():
@@ -60,12 +65,10 @@ def register():
                     except AttributeError:  
                 #else:
                         try:
-                            add_user = User(name=name, password=password, desc=" ",friends=[])
+                            add_user = User(name=name, password=password, desc=" ")
                             db.session.add(add_user)
-                            db.session.commit()
-                            session["name"] = name
-                            session["password"] = password
-                            return redirect(url_for("home", usr=name))
+                            db.session.commit() 
+                            return redirect(url_for("login"))
                         except IntegrityError:
                             flash("Password or Username is being used")
         else:
@@ -86,14 +89,63 @@ def home(usr):
             descrip = user.desc
             if view_name == user_name:
                 name = session["name"]
+                friends_user = Friends.query.filter_by(name=usr).all()
+                you_as_a_friend = Friends.query.filter_by(friends=usr).all()
+                friends_list = []
+                people_who_friended_you = []
+                try:
+                    for i in friends_user:
+                        friends_list.append(i.friends)
+                except AttributeError:
+                    pass
+                try:
+                    for i in you_as_a_friend:
+                        people_who_friended_you.append(i.name)
+                except AttributeError:
+                    pass
+                try:
+                    for i in people_who_friended_you:
+                        if i in friends_list:
+                            people_who_friended_you.remove(i)
+                except AttributeError:
+                    pass
                 if request.method == "POST":
                     description = request.form["desc_text"]
                     user.desc = description
                     db.session.commit()
                     return redirect(url_for("home", usr=name))
-                return render_template("profile_user.html", name=name, view_name=view_name, desc=descrip)
+                return render_template("profile_user.html", name=name, view_name=view_name, desc=descrip, friend=friends_list, pfriend=people_who_friended_you)
             else:
-                return render_template("profile_view.html", name=user_name, view_name=view_name, desc=descrip)
+                friend_or_unfriend = ""
+                view_user_friends = Friends.query.filter_by(name=view_name).all()
+                view_friends_list = []
+                try:
+                    for i in view_user_friends:
+                        view_friends_list.append(i.friends)
+                except AttributeError:
+                    pass
+                friends_user = Friends.query.filter_by(name=usr).all()
+                friends_list = []
+                try:
+                    for i in friends_user:
+                        friends_list.append(i.friends)
+                except AttributeError:
+                    pass
+                if usr in view_friends_list:
+                    friend_or_unfriend = "Unfriend"
+                else:
+                    friend_or_unfriend = "Friend"
+                if request.method == "POST":
+                    if usr not in view_friends_list:
+                        new_friend = Friends(name=view_name, friends=usr)
+                        db.session.add(new_friend)
+                        db.session.commit()
+                    else:
+                        del_friend = Friends.query.filter_by(name=view_name, friends=usr).first()
+                        db.session.delete(del_friend)
+                        db.session.commit()
+                    return redirect(url_for("home", usr=usr))
+                return render_template("profile_view.html", name=user_name, view_name=view_name, desc=descrip, friend=friends_list, friend_or=friend_or_unfriend)
         except AttributeError:
             return render_template("404.html")
     else:
